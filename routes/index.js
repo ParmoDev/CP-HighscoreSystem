622a5933da7ae4873c31e2d04d739b92aa6d72cb
Magnus Parmo <73383707+ParmoDev@users.noreply.github.com>
2022-03-01 07:09:09 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', (req, res) => {
-  fs.appendFileSync('./highscores.txt', req.body.username + ": " + req.body.highscore + "\n")
+router.post('/submit', (req, res) => {
+  if (!req.body.key || req.body.key != "CodingPirates") return res.send("Invalid key.");
+  if (!req.body.username || !req.body.score) return res.send('Please include username AND score.\nExample: {"key": "key", "username": "codingpirates", "score": 45}')
 
-  res.json(req.body.highscore + " added to highscore list!")
+  fs.appendFileSync('./highscores.txt', req.body.username + ": " + req.body.score + "\n")
+
+  res.json(req.body.score + " added to highscore list!")
+})
+
+router.get('/scores', (req, res) => {
+  fs.readFile('./highscores.txt', 'utf-8', (err, data) => {
+    res.send(data)
+  })
 })
 
 module.exports = router;
